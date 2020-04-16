68eeb9ca75778bed9cc6a7b561dc6b28ae61b615
Akins <smo244@gmail.com>
2020-04-16 12:16:31 -0700

    changed config for heroku

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://botanicalpilot:63QJ4ITTrnfG8sZd@cluster0-u7uuz.mongodb.net/test?retryWrites=true&w=majority"
+    url: "mongodb+srv://botanicalpilot:s8KH6PeKtlrwKAqv@cluster0-u7uuz.mongodb.net/test?retryWrites=true&w=majority"
 }
