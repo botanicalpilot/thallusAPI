c22b0fdf9115a1380c6317dd43249ee13f810ebd
Akins <smo244@gmail.com>
2020-04-16 15:54:37 -0700

    changes for heroku

---
+++ config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb+srv://botanicalpilot:U0o53p8Oc0ZwbO5M@cluster0-u7uuz.mongodb.net/test?retryWrites=true&w=majority"
+}
--- routes/thallus.routes.js
+++ routes/thallus.routes.js
@@ -3,8 +3,8 @@ module.exports = app => {
 
     var router = require("express").Router();
 
-    // create a new crop
-    router.post("/", crops.create);
+    // // create a new crop
+    // router.post("/", crops.create);
 
     // get all crops by common_name or scientific_name
     router.get("/", crops.findAll);
@@ -12,14 +12,14 @@ module.exports = app => {
     // get crop by id
     router.get("/:id", crops.findOne);
 
-    // update crop by id
-    router.put("/:id", crops.update);
+    // // update crop by id
+    // router.put("/:id", crops.update);
 
-    // delete crop by id
-    router.delete("/:id", crops.delete);
+    // // delete crop by id
+    // router.delete("/:id", crops.delete);
 
-    // delete all crops
-    router.delete("/", crops.deleteAll);
+    // // delete all crops
+    // router.delete("/", crops.deleteAll);
 
     app.use('/api/crops', router);
 };
