588be4342a7bb03d5cafa66b8dfea0fa4961150c
Akins <smo244@gmail.com>
2020-04-16 15:50:55 -0700

    removing routes for heroku

--- routes/thallus.routes.js
+++ routes/thallus.routes.js
@@ -3,23 +3,23 @@ module.exports = app => {
 
     var router = require("express").Router();
 
-    // create a new crop
-    router.post("/", crops.create);
+    // // create a new crop
+    // router.post("/", crops.create);
 
-    // get all crops by common_name
+    // get all crops by common_name or scientific_name
     router.get("/", crops.findAll);
 
     // get crop by id
     router.get("/:id", crops.findOne);
 
-    // update crop by id
-    router.put("/:id", crops.update);
+    // // update crop by id
+    // router.put("/:id", crops.update);
 
-    // delete crop by id
-    router.delete("/:id", crops.delete);
+    // // delete crop by id
+    // router.delete("/:id", crops.delete);
 
-    // delete all crops
-    router.delete("/", crops.deleteAll);
+    // // delete all crops
+    // router.delete("/", crops.deleteAll);
 
     app.use('/api/crops', router);
 };
