70c2249a16fedc7c629c99e0730d995e02a87d5c
Akins <smo244@gmail.com>
2020-04-16 12:20:30 -0700

    changed password again for heroku

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://botanicalpilot:s8KH6PeKtlrwKAqv@cluster0-u7uuz.mongodb.net/test?retryWrites=true&w=majority"
+    url: "mongodb+srv://botanicalpilot:2fSqHwn1RaKjOgvy@cluster0-u7uuz.mongodb.net/test?retryWrites=true&w=majority"
 }
