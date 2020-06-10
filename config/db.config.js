f481d98e84e1ab8de871bee62af69a853b7352d6
Akins <smo244@gmail.com>
2020-06-09 18:35:56 -0700

    removed mongodb from config

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url:"mongodb+srv://botanicalpilot:55yVKdbFmZHNtAg@cluster0-u7uuz.mongodb.net/test?retryWrites=true&w=majority"
+    url:"mongodb+srv://botanicalpilot:@cluster0-u7uuz.mongodb.net/test?retryWrites=true&w=majority"
 }
